1eeeeb108d7a5cbe4c075a46a1d2575a017d5b95
EthanSchatz <EthanSchatz@users.noreply.github.com>
2016-09-16 17:57:33 -0500

    Update TeamEvents.swift
    forgot to remove the print hehe

--- Sources/Camille/TeamEvents.swift
+++ Sources/Camille/TeamEvents.swift
@@ -6,7 +6,6 @@ public struct team_join: RTMAPIEvent {
     
     public static func make(withJson json: [String: Any], builderFactory: ([String: Any]) -> SlackModelBuilder) throws -> Parameters {
         let builder = builderFactory(json)
-        print("\n\n\n\(builder)\n\n\n")
         let user: User = try builder.value(build: "user")
         return user
     }
