2a325ca9129915bf10cec921d7e00c1741acfd4b
ethanschatzline <ethanschatzline@yahoo.com>
2016-09-21 23:28:56 -0500

    Split AnnouncementService into UserJoinService and TimedMessage Service

--- Sources/Camille/main.swift
+++ Sources/Camille/main.swift
@@ -6,20 +6,25 @@ let bot = try SlackBot(
     authenticator: TokenAuthentication.self,
     storage: RedisStorage.self,
     services: [
-        AnnouncementService(config: AnnouncerConfig { im in
-            return SlackMessage(target: im)
-                .text("Hi,")
-                .user(im.user)
-                .text(", welcome to the ios-developer slack team!")
-                .apiMethod()
-        }),
         HelloBot(),
         KarmaBot(options: KarmaBotOptions(
             targets: ["bot-laboratory"],
             addText: "++",
             removeText: "--",
             textDistanceThreshold: 4
-        ))
+        )),
+        TimedMessageService(config: TimedMessageConfig(interval: 3600.0, target: "intro") { target in
+            return SlackMessage(target: target)
+                .text("Welcome new team members!")
+                .apiMethod()
+        }),
+        UserJoinService(config: UserJoinConfig(newUserAnnouncement: { im in
+            return SlackMessage(target: im)
+                .text("Hi,")
+                .user(im.user)
+                .text(", welcome to the ios-developer slack team!")
+                .apiMethod()
+        }))
     ]
 )
 
