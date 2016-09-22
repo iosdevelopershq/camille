6ab6120c967d4b188c0e08e2df1f4984257deb08
ethanschatzline <ethanschatzline@yahoo.com>
2016-09-21 23:54:08 -0500

    PR Changes

--- Sources/Camille/TimedMessageService.swift
+++ Sources/Camille/TimedMessageService.swift
@@ -20,13 +20,14 @@ final class TimedMessageService: SlackRTMEventService {
     func configureEvents(slackBot: SlackBot, webApi: WebAPI, dispatcher: SlackRTMEventDispatcher) {
         dispatcher.onEvent(pong.self) { data in
             guard let timestamp = data["timestamp"] as? Int else { return }
-            let previous: TimeInterval = slackBot.storage.get(.in("Announcements"), key: "previousTimeStamp", or: 0)
-            if (TimeInterval(timestamp) - previous) >= self.config.interval {
-                try self.pongEvent(slackBot: slackBot, webApi: webApi, timestamp: TimeInterval(timestamp))
-            }
+            
+            try self.pongEvent(slackBot: slackBot, webApi: webApi, timestamp: TimeInterval(timestamp))
         }
     }
     func pongEvent(slackBot: SlackBot, webApi: WebAPI, timestamp: TimeInterval) throws {
+        let previous: TimeInterval = slackBot.storage.get(.in("Announcements"), key: "previousTimeStamp", or: 0)
+        guard (timestamp - previous) >= self.config.interval else { return }
+        
         try slackBot.storage.set(.in("Announcements"), key: "previousTimeStamp", value: timestamp)
         let data = slackBot.currentSlackModelData()
         guard let channel = data.channels.filter({ $0.name == config.target }).first
