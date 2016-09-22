c41726c404358d8eeed9daf2ad22fcdbe9f3e04c
ethanschatzline <ethanschatzline@yahoo.com>
2016-09-21 23:43:30 -0500

    PR Changes

--- Sources/Camille/TimedMessageService.swift
+++ Sources/Camille/TimedMessageService.swift
@@ -20,31 +20,20 @@ final class TimedMessageService: SlackRTMEventService {
     func configureEvents(slackBot: SlackBot, webApi: WebAPI, dispatcher: SlackRTMEventDispatcher) {
         dispatcher.onEvent(pong.self) { data in
             guard let timestamp = data["timestamp"] as? Int else { return }
-            
-            try self.pongEvent(slackBot: slackBot, webApi: webApi, timestamp: TimeInterval(timestamp))
+            let previous: TimeInterval = slackBot.storage.get(.in("Announcements"), key: "previousTimeStamp", or: 0)
+            if (TimeInterval(timestamp) - previous) >= self.config.interval {
+                try self.pongEvent(slackBot: slackBot, webApi: webApi, timestamp: TimeInterval(timestamp))
+            }
         }
     }
     func pongEvent(slackBot: SlackBot, webApi: WebAPI, timestamp: TimeInterval) throws {
-        let previous: TimeInterval = slackBot.storage.get(.in("Announcements"), key: "previousTimeStamp", or: 0)
-        if (timestamp - previous) >= config.interval {
-            try slackBot.storage.set(.in("Announcements"), key: "previousTimeStamp", value: timestamp)
-            let data = slackBot.currentSlackModelData()
-            guard let channel = data.channels.filter({ $0.name == config.target }).first
-                else { return }
-            
-            let message = config.announcement(channel)
-            
-            try webApi.execute(message)
-        }
-    }
-}
-
-extension Double: StorableType {
-    public var stringValue: String {
-        return String(self)
-    }
-    public static func makeValue(from string: String) throws -> Double {
-        guard let value = Double(string) else { throw StorageError.invalidValue(value: string) }
-        return value
+        try slackBot.storage.set(.in("Announcements"), key: "previousTimeStamp", value: timestamp)
+        let data = slackBot.currentSlackModelData()
+        guard let channel = data.channels.filter({ $0.name == config.target }).first
+            else { return }
+        
+        let message = config.announcement(channel)
+        
+        try webApi.execute(message)
     }
 }
