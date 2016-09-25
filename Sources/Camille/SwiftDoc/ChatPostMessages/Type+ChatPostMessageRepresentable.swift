d5260428a0f964f16e2b6cef63e4c34fb2aa2175
Ian Keen <iankeen82@gmail.com>
2016-09-24 22:30:54 -0700

    updated to new Sugar syntax

--- Sources/Camille/SwiftDoc/ChatPostMessages/Type+ChatPostMessageRepresentable.swift
+++ Sources/Camille/SwiftDoc/ChatPostMessages/Type+ChatPostMessageRepresentable.swift
@@ -1,101 +1,37 @@
+import Bot
+import Sugar
 import Models
-import WebAPI
 
-extension Type: ChatPostMessageRepresentable {
-    func makeChatPostMessage(target: SlackTargetType) throws -> ChatPostMessage {
-        let type = self.name.capitalized
-        let fallback = "<http://swiftdoc.org/type/\(type)/|\(type)>"
+
+extension Type: InteractiveButtonChatPostMessageRepresentable {
+    public func makeInteractiveButtonChatPostMessage(target: SlackTargetType, responder: SlackInteractiveButtonResponderService, handler: @escaping (InteractiveButtonResponse) throws -> Void) throws -> ChatPostMessage {
         
-        var json: [String: Any] = [
-            "fallback": fallback,
-            "text": "",
-            ]
-        var buttonAttachment = try MessageAttachment.makeModel(with: SlackModelBuilder.make(json: json))
-        buttonAttachment.fields = try self.buttonFields()
-        buttonAttachment.actions = try self.buttonActions()
+        let message = SlackMessage()
+            .line("Details of:", self.name.capitalized.bold)
+            .attachment { builder in
+                
+                let buttonMap: [String: [Any]] = [
+                    "Inits": self.inits,
+                    "Functions": self.functions,
+                    "Properties": self.properties,
+                    "Subscripts": self.subscripts,
+                    "Inherits": self.allInherits,
+                    "Inherited": self.allInherited,
+                    ]
+                
+                let buttons: [String] = buttonMap.flatMap { item -> String? in
+                    return (item.value.isEmpty ? nil : item.key)
+                }
+                
+                builder.field(short: true, title: "Type", value: self.kind)
+                for button in buttons {
+                    builder.button(name: button, text: button, responder: responder, handler: handler)
+                }
+            }
+//            .attachment { builder in
+//                //comment
+//        }
         
-        json["text"] = self.comment
-        json["mrkdwn_in"] = ["text"]
-        let commentAttachment = try MessageAttachment.makeModel(with: SlackModelBuilder.make(json: json))
-        
-        return ChatPostMessage(
-            target: target,
-            text: "Details of: *\(type)*",
-            attachments: [buttonAttachment, commentAttachment]
-        )
-    }
-    
-    func buttonFields() throws -> [MessageAttachmentField] {
-        var fields = [MessageAttachmentField]()
-        
-        fields.append(try MessageAttachmentField.makeModel(with: SlackModelBuilder.make(json: [
-            "title": "Type",
-            "value": self.kind,
-            "short": false
-            ]))
-        )
-        
-        return fields
-    }
-    func buttonActions() throws -> [MessageAttachmentAction] {
-        var buttons = [MessageAttachmentAction]()
-        
-        if !self.inits.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Initalisers",
-                "text": "Initalisers",
-                "value": "Initalisers",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        if !self.functions.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Functions",
-                "text": "Functions",
-                "value": "Functions",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        if !self.properties.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Properties",
-                "text": "Properties",
-                "value": "Properties",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        if !self.subscripts.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Subscripts",
-                "text": "Subscripts",
-                "value": "Subscripts",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        
-        if !self.allInherits.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Inherits",
-                "text": "Inherits",
-                "value": "Inherits",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        if !self.allInherited.isEmpty {
-            let buttonJson: [String: Any] = [
-                "name": "Inherited",
-                "text": "Inherited",
-                "value": "Inherited",
-                "type": "button",
-                ]
-            buttons.append(try MessageAttachmentAction.makeModel(with: SlackModelBuilder.make(json: buttonJson)))
-        }
-        
-        return buttons
+        return try message.makeChatPostMessage(target: target)
     }
 }
--- Sources/Camille/SwiftDoc/SwiftDocService.swift
+++ Sources/Camille/SwiftDoc/SwiftDocService.swift
@@ -1,9 +1,12 @@
 import Bot
 import Sugar
 
-class SwiftDocService: SlackMessageService, SlackConnectionService {
+class SwiftDocService: SlackMessageService, SlackInteractiveButtonResponderService, SlackConnectionService {
+    public let buttonResponder = SlackInteractiveButtonResponder()
+
     private var sync: SwiftDocSync?
     
+    //MARK: - Connection
     func connected(slackBot: SlackBot, botUser: BotUser, team: Team, users: [User], channels: [Channel], groups: [Group], ims: [IM]) throws {
         self.sync = SwiftDocSync(
             http: slackBot.http,
@@ -12,6 +15,7 @@ class SwiftDocService: SlackMessageService, SlackConnectionService {
         try self.sync?.updateDataset()
     }
     
+    //MARK: - Messages
     override func messageEvent(slackBot: SlackBot, webApi: WebAPI, message: MessageDecorator, previous: MessageDecorator?) throws {
         guard let sync = self.sync, let target = message.target else { return }
         
@@ -22,20 +26,29 @@ class SwiftDocService: SlackMessageService, SlackConnectionService {
     }
 }
 
+//MARK: - Docs
 fileprivate extension SwiftDocService {
     func showDocs(in target: SlackTargetType, webApi: WebAPI, sync: SwiftDocSync) -> (PatternMatchResult) throws -> Void {
         return { match in
             let type: String = match.value(named: "type")
             
             do {
-                guard let data = try sync.lookup(item: type) as? ChatPostMessageRepresentable
+                guard let data = try sync.lookup(item: type) as? InteractiveButtonChatPostMessageRepresentable
                     else { throw SwiftDocError.unableToDisplay(item: type) }
                 
-                try webApi.execute(try data.makeChatPostMessage(target: target))
+                let message = try data.makeInteractiveButtonChatPostMessage(
+                    target: target,
+                    responder: self,
+                    handler: { response in
+                        print(response)
+                    }
+                )
+                
+                try webApi.execute(message)
                 
             } catch let error {
-                let reply = SlackMessage(target: target).text(String(describing: error))
-                try webApi.execute(reply.apiMethod())
+                let reply = SlackMessage().add(segment: String(describing: error))
+                try webApi.execute(reply.makeChatPostMessage(target: target))
             }
         }
     }
